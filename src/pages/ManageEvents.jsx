d4b205325d5c8baf641f109645443e18352edd75
renielnash <nashreniel@gmail.com>
2026-03-25 15:55:31 +0800

    show events in manage events tab

--- src/pages/ManageEvents.jsx
+++ src/pages/ManageEvents.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import MainLayout from "../layout/MainLayout";
 import { Link } from "react-router";
+import { supabase } from "../Utils/supabase";
+import { useEffect, useState } from "react";
+import EventCard from "../components/EventCard";
 
 const ManageEvents = () => {
+    const [events, setEvents] = useState(null);
+    useEffect(() => {
+        const fetchEvents = async () => {
+            const { data: eventsData, error: eventsError } = await supabase
+                .from("events")
+                .select();
+            if (eventsError) alert(eventsError)
+            if (eventsData) setEvents(eventsData);
+        };
+
+        fetchEvents();
+    }, []);
     return (
         <MainLayout>
             <div className="pt-5">
-                <div>We load the events here</div>
                 <div className="text-right">
                     <Link to="/add-event" className="btn btn-primary rounded-full">
                         Add Event
                     </Link>
+
+                </div>
+                <div>
+                    {events?.map((event) => {
+                        return <EventCard event={event} />;
+
+                    })}
                 </div>
             </div>
         </MainLayout>
